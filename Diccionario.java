a03ea868f3a892fe21a0f452c6d2fcc792731d06
Joaco <joacofurze@gmail.com>
2026-06-05 15:36:45 -0300

    Diccionario corregido BIEN

--- Diccionario.java
+++ Diccionario.java
@@ -1,90 +1,65 @@
 public class Diccionario<K, V> implements IColeccion {
 
-    private static final int CAPACIDAD = 16;
+    private static final int CAPACIDAD = 100;
 
     private class Entrada {
         K clave;
         V valor;
-        Entrada siguiente;
-
-        Entrada(K clave, V valor) {
-            this.clave = clave;
-            this.valor = valor;
-        }
     }
 
-    private final Object[] tabla;
-    private int cantidad;
+    private final Entrada[] datos;
+    private int cant;
 
+    @SuppressWarnings("unchecked")
     public Diccionario() {
-        tabla = new Object[CAPACIDAD];
+        datos = new Diccionario.Entrada[CAPACIDAD];
+        for (int i = 0; i < CAPACIDAD; i++) datos[i] = new Entrada();
     }
 
-    private int calcularPosicion(K clave) {
-        int codigoHash = clave.hashCode();
-        if (codigoHash < 0) codigoHash = -codigoHash;
-        return codigoHash % CAPACIDAD;
+    private int buscarPosicion(K claveBuscada) {
+        int i = 1;
+        while (i <= cant) {
+            if (datos[i].clave.equals(claveBuscada)) return i;
+            i++;
+        }
+        return -1;
     }
 
     public void insertar(K clave, V valor) {
-        int posicion = calcularPosicion(clave);
-        @SuppressWarnings("unchecked")
-        Entrada entradaActual = (Entrada) tabla[posicion];
-
-        while (entradaActual != null) {
-            if (entradaActual.clave.equals(clave)) {
-                entradaActual.valor = valor;
-                return;
-            }
-            entradaActual = entradaActual.siguiente;
+        int pos = buscarPosicion(clave);
+        if (pos != -1) {
+            datos[pos].valor = valor;
+            return;
         }
-
-        Entrada nuevaEntrada = new Entrada(clave, valor);
-        @SuppressWarnings("unchecked")
-        Entrada primera = (Entrada) tabla[posicion];
-        nuevaEntrada.siguiente = primera;
-        tabla[posicion] = nuevaEntrada;
-        cantidad++;
+        cant++;
+        datos[cant].clave = clave;
+        datos[cant].valor = valor;
     }
 
     public V buscar(K clave) {
-        int posicion = calcularPosicion(clave);
-        @SuppressWarnings("unchecked")
-        Entrada entradaActual = (Entrada) tabla[posicion];
-
-        while (entradaActual != null) {
-            if (entradaActual.clave.equals(clave)) return entradaActual.valor;
-            entradaActual = entradaActual.siguiente;
-        }
+        int pos = buscarPosicion(clave);
+        if (pos != -1) return datos[pos].valor;
         return null;
     }
 
     public boolean contiene(K clave) {
-        return buscar(clave) != null;
+        return buscarPosicion(clave) != -1;
     }
 
     public boolean eliminar(K clave) {
-        int posicion = calcularPosicion(clave);
-        @SuppressWarnings("unchecked")
-        Entrada entradaActual = (Entrada) tabla[posicion];
-        Entrada entradaAnterior = null;
-
-        while (entradaActual != null) {
-            if (entradaActual.clave.equals(clave)) {
-                if (entradaAnterior == null) tabla[posicion] = entradaActual.siguiente;
-                else entradaAnterior.siguiente = entradaActual.siguiente;
-                cantidad--;
-                return true;
-            }
-            entradaAnterior = entradaActual;
-            entradaActual = entradaActual.siguiente;
-        }
-        return false;
+        int pos = buscarPosicion(clave);
+        if (pos == -1) return false;
+        datos[pos].clave = datos[cant].clave;
+        datos[pos].valor = datos[cant].valor;
+        datos[cant].clave = null;
+        datos[cant].valor = null;
+        cant--;
+        return true;
     }
 
     @Override
-    public boolean estaVacia() { return cantidad == 0; }
+    public boolean estaVacia() { return cant == 0; }
 
     @Override
-    public int getCantidad() { return cantidad; }
+    public int getCantidad() { return cant; }
 }
